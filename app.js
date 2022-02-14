bcd221e125576607a64b69d29e627536e4f9cfdf
Fethi9an <martinfethi97@hotmail.com>
2022-02-14 10:51:17 +0100

    *

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const cors = require("cors")
-const express =require("express");
-const req = require("express/lib/request");
+const cors= require("cors")
+const express= require("express")
 
-const app = express()
+const app=express()
 const PORT = process.env.PORT || 3000
-app.use('/healtcheck',require('./routes/healthcheck.routes'));
-app.use(express.urlencoded({ extended: true }));
+
+app.use('/healthcheck' , require('./routes/healthcheck.routes'));
+app.use(express.urlencoded({extended: true}));
 app.use(cors())
 
-app.get("/", (req, res)=>{
-    headers={"http_status":200, "cache control": "no-cache"}
+app.get("/" , (req ,res)=>{
+    Headers={"http_status":200, "cache-control": "no-cache"}
     body={"status": "available"}
     res.status(200).send(body)
 })
 
 app.listen(PORT , ()=>{
-    console.log('STARTED LISTENING ON PORT ${PORT}')
+    console.log('STARTED LISTENING ON PORT ${PORT} ')
 });
+
